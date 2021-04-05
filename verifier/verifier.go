741c5ea96d457d702a68e74671c4e6ba543222d2
Roger Coll <roger.coll.aumatell@gmail.com>
2021-04-05 18:36:45 +0200

    Add Verifer and Optimistic Contracts

--- verifier/verifier.go
+++ verifier/verifier.go
@@ -1,1 +1,35 @@
 package verifier
+
+import (
+	"crypto/ecdsa"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/rogercoll/optimisticrp"
+)
+
+type VerifierNode struct {
+	accountsTrie optimisticrp.Optimistic
+	ethContract  optimisticrp.OptimisticSContract
+	privKey      *ecdsa.PrivateKey
+	onChainRoot  common.Hash
+}
+
+func New(newAccountsTrie optimisticrp.Optimistic, newEthContract optimisticrp.OptimisticSContract, privateKey *ecdsa.PrivateKey) *VerifierNode {
+	return &VerifierNode{
+		accountsTrie: newAccountsTrie,
+		ethContract:  newEthContract,
+		privKey:      privateKey,
+	}
+}
+
+//Generate proof data to be send onchain, a proof proves that key with a certain value exits on the trie
+func (v *VerifierNode) generateProof(acc common.Address) {
+	_, err := v.accountsTrie.NewProve(acc)
+	if err != nil {
+		return
+	}
+}
+
+func (v *VerifierNode) VerifyOnChainData(logs chan<- string) {
+
+}
